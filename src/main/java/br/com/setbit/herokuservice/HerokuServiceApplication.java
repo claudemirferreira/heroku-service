c1cf4e889cfc656c4d87f9ef96e923dcb60bd7ab
claudemirferreira <claudemirramosferreira@gmail.com>
2021-10-23 20:11:36 -0300

    commit initial

---
+++ src/main/java/br/com/setbit/herokuservice/HerokuServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.setbit.herokuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/setbit/herokuservice/HerokuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.setbit.herokuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
